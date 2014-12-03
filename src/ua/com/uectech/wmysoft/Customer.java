7f655060c87a7c7329de03036ec2111e135bbfb0
xtfkpi <xtfkpi@yandex.ru>
2014-12-03 16:56:05 +0200

    jtest added

--- src/ua/com/uectech/wmysoft/Customer.java
+++ src/ua/com/uectech/wmysoft/Customer.java
@@ -4,9 +4,21 @@ public class Customer {
 	String name;
 	int id;
 	int money;
+	
+	Customer(int i,String n,int m){
+		this.id=i;
+		this.name=n;
+		this.money=m;
+	}
+	
+	void display(){
+		System.out.println("Hello, my name is "+this.name+" and I have $"+this.money);
+	}
 
 	public static void main(String[] args) {
-
+		Customer c1 = new Customer(1,"Serg Larin",30000);
+		Customer c2 = new Customer(2,"Serg Krasyuk",40000);
+		c1.display();
 	}
 
 }
---
+++ src/ua/com/uectech/wmysoft/CustomerTest.java
@@ -0,0 +1,15 @@
+package ua.com.uectech.wmysoft;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerTest {
+
+	@Test
+	public void testCustomer() {
+		System.out.println("very good");
+		//fail("Not yet implemented");
+	}
+
+}
