b7568321e231c76439903e53860bf7d7df9d3f3b
xtfkpi <xtfkpi@yandex.ru>
2014-12-04 12:33:29 +0200

    it saves

--- src/ua/com/uectech/wmysoft/CustomerTest.java
+++ src/ua/com/uectech/wmysoft/CustomerTest.java
@@ -2,6 +2,10 @@ package ua.com.uectech.wmysoft;
 
 import static org.junit.Assert.*;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import org.junit.Test;
 
 public class CustomerTest {
@@ -11,5 +15,29 @@ public class CustomerTest {
 		System.out.println("very good");
 		//fail("Not yet implemented");
 	}
-
+	@Test
+	public void testSaveCustomer(){
+		Customer max = new Customer("Max Gurvits",30000);
+			
+		max.wl[0]=new TodoItem(new Project("JobHive"),new Manager("Dr. Michael Feinberg"));
+		max.wl[1]=new TodoItem(new Project("Little Bird"),new Manager("Arkadius Dobkin"));
+		max.wl[2]=new TodoItem(new Project("SendHub"),new Manager("Dmitriy Kushnir"));
+		max.wl[3]=new TodoItem(new Project("Vacatia"),new Manager("Vladimir Sharov"));
+		max.projectsNumber=4;
+		
+		max.display();	
+		
+		try {
+		ObjectOutputStream cs=new ObjectOutputStream(new FileOutputStream("cs.ser"));
+		cs.writeObject(max);
+		cs.flush();
+		cs.close();
+		} catch (IOException e){
+			
+		}
+		finally {
+			System.out.println("saved? ");
+		}
+		
+	}
 }
